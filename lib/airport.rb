dbf863579d9b8e78f11432698dc79cc150c16d8b
jack.willee <jackwillee1995@gmail.com>
2021-03-16 15:26:37 +0100

    user story 2

--- lib/airport.rb
+++ lib/airport.rb
@@ -4,5 +4,8 @@ def land(plane)
 
 
 end 
+def take_off(plane)
+@plane = plane
+end
 
 end 
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,11 +1,16 @@
 require 'airport.rb'
 describe Airport do 
+    
+    subject(:airport) {described_class.new}
 
 it 'should be able to respond to land method' do 
-expect(subject).to respond_to(:land).with(1).argument
+expect(airport).to respond_to(:land).with(1).argument
 
 
 end 
 
+it 'should be able to respond to take off method' do 
+    expect(airport).to respond_to(:take_off).with(1).argument
 
+end 
 end 
