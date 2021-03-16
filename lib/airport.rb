9e3e7822f4e3d8bbcee3d70aaefbd7c9ebb047d9
jack.willee <jackwillee1995@gmail.com>
2021-03-16 18:00:30 +0100

    user story 3

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,13 @@
 class Airport
-
-def land(plane)
-
-
+    
+def initialize(capacity)
+@capacity = capacity
+@planes = []
 end 
+def land(plane)
+    raise 'no space' if @planes.length >= @capacity
+    @planes << plane
+  end
 def take_off(plane)
 @plane = plane
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,7 @@
 require 'airport.rb'
 describe Airport do 
     
-    subject(:airport) {described_class.new}
+    subject(:airport) {described_class.new(15)}
 
 it 'should be able to respond to land method' do 
 expect(airport).to respond_to(:land).with(1).argument
@@ -13,4 +13,11 @@ it 'should be able to respond to take off method' do
     expect(airport).to respond_to(:take_off).with(1).argument
 
 end 
+it 'does not allow plane to land if no space' do
+    plane = double :plane
+    15.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane) }.to raise_error 'no space'
+  end
 end 
